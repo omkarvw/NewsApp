c88f3d63cf0bfb203719f9492a5060c291fab33b
Omkar Wadekar <113306973+omkarvw@users.noreply.github.com>
2023-05-14 15:44:17 +0530

    Update NewsItem.js

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -6,22 +6,22 @@ const NewsItem = (props) => {
         <>
 
             <div className="card" >
-                <div style={{
-                    display: 'flex',
-                    justifyContent: 'flex-end',
-                    position: 'absolute',
-                    right: '0'
-                }}>
-                    <span className="badge rounded-pill bg-danger" >
-                        {source}
-                    </span>
-                </div>
-                <img src={!imageUrl ? "https://a.mortgagenewsdaily.com/dsocial/63238f9d9c51c45985fb5dcb/63238f9d9c51c45985fb5dcb_0_202209160455.jpg?provider=facebook" : imageUrl} className="card-img-top" alt="..." />
+                <img src={imageUrl ? imageUrl : "https://images.unsplash.com/photo-1585829365295-ab7cd400c167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"} className="card-img-top" alt="" />
                 <div className="card-body">
                     <h5 className="card-title">{title} </h5>
                     <p className="card-text">{description}</p>
                     <p className="card-text"><small className="text-muted">{author} On {new Date(date).toGMTString()}</small></p>
                     <a href={newsUrl} target="blank" className="btn btn-sm btn-dark">Read More</a>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0',
+                    }}>
+                        <span className="badge rounded-pill bg-danger" >
+                            {source}
+                        </span>
+                    </div>
                 </div>
             </div>
         </>
